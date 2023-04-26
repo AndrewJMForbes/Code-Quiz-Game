cb418b55cb40bd46ae924a3785504144d386ca91
Forbes85 <vudo8585@gmail.com>
2023-04-26 10:13:43 -0500

    edited HTML, CSS, and JS 4-26 10:13

--- script.js
+++ script.js
@@ -1,94 +1,124 @@
 let questionIndex = 0;
+let answersIndex = 0;
 const answerArray = [];
-var startGame = document.querySelector('#start');
-const answerA = document.querySelector('#answerA');
-const answerB = document.querySelector('#answerB');
-const answerC = document.querySelector('#answerC');
-const answerD = document.querySelector('#answerD');
+
+
 
 function onStart() {
-  console.log('clicked on start')
+  console.log("clicked on start");
 
-const welcome = document.getElementById('welcome');
-  welcome.style.display= 'none';
-  const questionContainer = document.getElementById('question-container');
-  questionContainer.style.display= 'block';
-  const questionEl = document.getElementById('quiz-question');
+  const welcome = document.getElementById("welcome");
+  welcome.style.display = "none";
+  const questionContainer = document.getElementById("question-container");
+  questionContainer.style.display = "block";
   // eventually make its own function
-  const questionObj = quizQuestion[questionIndex];
-  questionEl.innerText = questionObj.question;
-  let answerlbl = document.getElementById('answerAlbl');
-  answerlbl.innerText = questionObj.answers[0];
-  answerlbl = document.getElementById('answerBlbl');
-  answerlbl.innerText = questionObj.answers[1];
-  answerlbl = document.getElementById('answerClbl');
-  answerlbl.innerText = questionObj.answers[2];
-  answerlbl = document.getElementById('answerDlbl');
-  answerlbl.innerText = questionObj.answers[3];
+  loadQuestion();
 }
-
+function getResponse() {
+  const optionAEl = document.getElementById('answerA');
+  const optionBEl = document.getElementById('answerB');
+  const optionCEl = document.getElementById('answerC');
+  const optionDEl = document.getElementById('answerD');
+  let response = '';
+  if(optionAEl.checked === true) {
+    response = optionAEl.value;
+  } else if (optionBEl.checked === true) {
+    response = optionBEl.value;
+  } else if (optionCEl.checked === true) {
+    response = optionCEl.value;
+  } else if (optionDEl.checked === true) {
+    response = optionDEl.value;
+  }
+  return response;
+}
 function onSubmit() {
-  // figure out what what was selected
-    var rightAnswer = ['0', '3', '3']
-  for (var i = 0; i < quizQuestion.length; i++) {
-    if (rightAnswer == ['0']) {
-      continue;
-    }
-
   
-  // what is the right answer
-  // compare whether their answer was correct or not
+  // figure out what what was selected
+  const result = checkAnswer();
+  console.log('was the answer correct? ', result)
   // store whether they answered correctly
   // move onto the next question
-
-  function loadQuestion() {;
-
-  }
   questionIndex++;
   loadQuestion();
-  onSubmit();
 }
+function checkAnswer() {
+  const response = getResponse();
+  console.log('user chose ', response);
+  // what is the right answer
+  const correctAnswer = quizQuestions[questionIndex].correctAnswer;
+  console.log('right', correctAnswer);
+
+  // compare whether their answer was correct or not
+  return (response === correctAnswer) 
+    
 }
 
-var quizQuestion = [{
-  question:'What color is the sky?',
-  answers : ['Blue', 'Yellow', 'Purple', 'Green'],
-  correctAnswer: 0
-  
-},
-{
-  question: 'How many letters are in the word Friday',
-  answers : [32, 12 , 3, 6],
-  correctAnswer : 3
-},
-{
-  question: 'What does the H in HTML stand for?',
-  answers : ['hardhat', 'happy', 'hyperbeam', 'hyper'],
-  correctAnswer: 3
-  
+function loadQuestion() {
+  if (questionIndex < quizQuestions.length) {
+    const questionEl = document.getElementById("quiz-question");
+    const questionObj = quizQuestions[questionIndex];
+    questionEl.innerText = questionObj.question;
+    let answerLbl = document.getElementById("answerAlbl");
+    answerLbl.innerText = questionObj.answers[0];
+    answerLbl = document.getElementById("answerBlbl");
+    answerLbl.innerText = questionObj.answers[1];
+    answerLbl = document.getElementById("answerClbl");
+    answerLbl.innerText = questionObj.answers[2];
+    answerLbl = document.getElementById("answerDlbl");
+    answerLbl.innerText = questionObj.answers[3];
+  } else {
+    showResults();
+  }
+
 }
+
+function showResults(){
+  // hide questions
+  const questionContainerEl = document.getElementById('question-container');
+  questionContainerEl.style.display = 'none';
+  //display results
+  const resultsContainerEl = document.getElementById('results-container');
+  resultsContainerEl.style.display = 'block';
+
+  questionIndex = 0;
+}
+const quizQuestions = [
+  {
+    question: "What color is the sky?",
+    answers: ["Blue", "Yellow", "Purple", "Green"],
+    correctAnswer: 'A',
+  },
+  {
+    question: "How many letters are in the word Friday",
+    answers: [32, 12, 3, 6],
+    correctAnswer: 'D',
+  },
+  {
+    question: "What does the H in HTML stand for?",
+    answers: ["hardhat", "happy", "hyperbeam", "hyper"],
+    correctAnswer: 'D',
+  },
 ];
 // console.log(quizQuestions);
-  
 
-answerA.addEventListener('click', function(event){
-  event.preventDefault;
-  console.log('#answerA')
-})
-answerB.addEventListener('click', function(event){
-  event.preventDefault;
-  console.log('#answerB')
-})
-answerC.addEventListener('click', function(event){
-  event.preventDefault;
-  console.log('#answerC')
-})
-answerD.addEventListener('click', function(event){
-  event.preventDefault;
-  console.log('#answerD')
-})
-startGame.addEventListener('click', function(event) {
-  event.preventDefault;
+// answerA.addEventListener("click", function (event) {
+//   event.preventDefault;
+//   console.log("#answerA");
+// });
+// answerB.addEventListener("click", function (event) {
+//   event.preventDefault;
+//   console.log("#answerB");
+// });
+// answerC.addEventListener("click", function (event) {
+//   event.preventDefault;
+//   console.log("#answerC");
+// });
+// answerD.addEventListener("click", function (event) {
+//   event.preventDefault;
+//   console.log("#answerD");
+// });
+// startGame.addEventListener("click", function (event) {
+//   event.preventDefault;
 
-  console.log(start)
-})
+//   console.log(start);
+// });
